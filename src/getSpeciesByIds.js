62e5713368a6fa717b139f47a688b34ef1fe1eab
polidex <lp.igor@gmail.com>
2022-04-16 15:16:49 -0300

    request 1 done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  const result = ids.map((id) => data.species.filter((specie) => specie.id === id));
+  return result.reduce((idA, idB) => [...idA, ...idB], []);
 }
-
 module.exports = getSpeciesByIds;
