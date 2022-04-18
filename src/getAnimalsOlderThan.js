9843096b6ef3bae47e6337e626ca05b742ef3666
polidex <lp.igor@gmail.com>
2022-04-18 17:35:39 -0300

    request 2 done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalName = data.species.find((specie) => specie.name === animal);
+  return animalName.residents.every((resident) => resident.age > age);
 }
-
 module.exports = getAnimalsOlderThan;
