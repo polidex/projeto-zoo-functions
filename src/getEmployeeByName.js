7717ef3a44e4c1f6abd90ee0b5e660b4721f9bcf
polidex <lp.igor@gmail.com>
2022-04-19 15:08:59 -0300

    tendo dificuldades

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const firstName = data.employees.map((employee) => employee.firstName);
+  const lastName = data.employees.map((employee) => employee.lastName);
+  const checkName = () => firstName || lastName === employeeName;
+  const result = data.employees.reduce((checkName), {});
+  return result;
+  // return checkName;
 }
-
+console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,11 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const checkManager = data.employees.map((employee) => employee.managers);
+  return checkManager? 
 }
+
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
