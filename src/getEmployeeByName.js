06233e9d5c2f3dfc30207d58a75dee47514ceaa7
polidex <lp.igor@gmail.com>
2022-04-21 01:54:38 -0300

    request 3 done

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,12 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  const firstName = data.employees.map((employee) => employee.firstName);
-  const lastName = data.employees.map((employee) => employee.lastName);
-  const checkName = () => firstName || lastName === employeeName;
-  const result = data.employees.reduce((checkName), {});
-  return result;
-  // return checkName;
+function getEmployeeByName(employeeName = {}) {
+  const result = data.employees.find(
+    (employee) =>
+      employee.firstName === employeeName || employee.lastName === employeeName,
+  );
+  return !result ? {} : result;
 }
-console.log(getEmployeeByName('Emery'));
+console.log(getEmployeeByName('Orloff'));
 module.exports = getEmployeeByName;
