8d1e1397ca13a5cbe044c936d37c7a07b7bcbd0e
polidex <lp.igor@gmail.com>
2022-04-23 21:28:03 -0300

    request 5 done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = {}) {
+  const defaultResult = {};
+
+  if (animal.specie && animal.sex) {
+    const subject = data.species.find((specie) => specie.name === animal.specie);
+    const result = subject.residents.filter((creature) => creature.sex === animal.sex);
+    return result.length;
+  }
+  if (animal.specie) {
+    const subject = data.species.find((specie) => specie.name === animal.specie);
+    return subject.residents.length;
+  }
+  data.species.forEach((specie) => { defaultResult[specie.name] = specie.residents.length; });
+  return defaultResult;
 }
 
+console.log(countAnimals());
 module.exports = countAnimals;
