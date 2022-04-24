d5e05de0907139059e5e92dbd88aad605c7a7cc0
polidex <lp.igor@gmail.com>
2022-04-24 17:21:59 -0300

    9 done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstManaged = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const specieObj = data.species.find((specie) => specie.id === firstManaged);
+  const isOlder = specieObj.residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(isOlder);
 }
 
 module.exports = getOldestFromFirstSpecies;
