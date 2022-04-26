35c8c309635b28c72e55a717cc7439e7d25e9962
polidex <lp.igor@gmail.com>
2022-04-26 00:47:57 -0300

    8 caminhando

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,27 @@
 const data = require('../data/zoo_data');
 
 const species = data.species; 
+const hours = data.hours;
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
 
-function filterByAnimal(nomeAnimal) {
-  return species.filter((specie) => specie.name === nomeAnimal);
+function findAnimal(nomeAnimal) {
+  const animal = species.find((specie) => specie.name === nomeAnimal);
+  return animal.availability;
 }
 
-console.log(filterByAnimal(`frogs`));
+function findWeekDay(weekDay) {
+  //linha 17: como filtrar um array dentro de um array e pke filtro dentro do filtro nao deu certo. pesqusiar includes.
+  const animals = species.filter((specie) => specie.availability.filter((day) => day === weekDay))
+  const seioq = { [weekDay]: {
+    officeHour: `Open from ${hours[weekDay].open}am until ${hours[weekDay].close}pm`,
+    exhibition: 'test',
+  }}
+  return animals; //hours[weekDay];
+}
+
+console.log(findWeekDay('Wednesday'));
 
 module.exports = getSchedule;
