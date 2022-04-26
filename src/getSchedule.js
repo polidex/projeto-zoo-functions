daa5fd44f1b6ac25e8487a365af6ac49ecbf372b
Igor Lungato Polido <93943042+polidex@users.noreply.github.com>
2022-04-25 23:53:57 -0300

    8 init

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const species = data.species; 
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
+
+function filterByAnimal(nomeAnimal) {
+  return species.filter((specie) => specie.name === nomeAnimal);
+}
+
+console.log(filterByAnimal(`frogs`));
 
 module.exports = getSchedule;
